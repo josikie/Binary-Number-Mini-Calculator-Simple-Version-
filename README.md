d188e8ef6ad0acd9fe67b82eb3277b336223e052
Josi Kie <kiejosi12@gmail.com>
2022-12-26 18:57:12 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## Binary Number Mini Calculator (Simple Version on C++)
+
+### Introduction
+
+Binary Number Mini Calculator is an application that can calculate addition of two binary numbers for you. A new binary number will appear as the result of the addition. This is the simple version of Binary Number Mini Calculator that I've been working on lately. However, this application built with C++. The Binary Number Mini Calculater is not build by C++.
+
+### Run locally
+
+1. Make sure you have g++ compiler on your PC. Check it with this command:
+```bash
+    g++ --version
+```
+2. Check your C++ version whether its C++11, C++14, or C++17, and so on : [YouTube](https://www.youtube.com/watch?v=7n6vGnJ7B8E). I prefer C++11 and above C++11.
+
+3. Clone this project. 
+
+4. Open terminal within the cloned project folder. 
+
+5. Run this command on the terminal:
+```bash
+    g++ addBinaryNumbers.cpp
+```
+Windows:
+```bash
+    ./a.exe
+```
+Mac/Linux/Unix based OS:
+```bash
+    ./a.out
+```
+
+You can change the binary numbers at addBinaryNumbers.cpp `std::cout << cbn.addBinaryNumbers("1011", "10111");`.
+
+### Contributors
+Josi Kie
---
+++ addBinaryNumbers.cpp
@@ -0,0 +1,141 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+class CalculateBinaryNumbers{
+    public:
+    long long int addBinaryNumbers(std::string a, std::string b){
+        // take the string lengths
+        long long int lengthA = a.length();
+        long long int lengthB = b.length();
+
+        // initiate pointer char array with size corresponding to the string lengths
+        char *arrA = new char[lengthA+1];
+        char *arrB = new char[lengthB+1];
+
+        // convert strings to char arrays
+        strcpy_s(arrA, lengthA+1, a.c_str());
+        strcpy_s(arrB, lengthB+1, b.c_str());
+
+        // convert char arrays to vector arrays
+        std::vector<char> charArrA;
+        for(long long int i = 0; i < lengthA; i++){
+            charArrA.push_back(arrA[i]);
+        }
+
+        std::vector<char> charArrB;
+        for(long long int i = 0; i < lengthB; i++){
+            charArrB.push_back(arrB[i]);
+        }
+
+        // delete memory for char arrays
+        delete[] arrA;
+        delete[] arrB;
+
+        // add 0 in front if length B less than length A
+        if(lengthA > lengthB){
+            long long int devitation = lengthA - lengthB;
+            for(long long int i = 0; i < devitation; i++){
+                charArrB.insert(charArrB.begin(), '0');
+            }
+        }
+
+        if(lengthB > lengthA){
+            long long int devitation = lengthB - lengthA;
+            for(long long int i = 0; i < devitation; i++){
+                charArrA.insert(charArrA.begin(), '0');
+            }
+        }
+
+        // reverse, add binary strings
+        std::vector<char> charAddAB;
+        char temp = '0';
+        for(long long int i = charArrA.size()-1; i > -1; i--){
+            char number = '0';
+            if(charArrA[i] == '1' && charArrB[i] == '1'){
+                
+                if (temp == '1' && i == 0){
+                    number = '1';
+                    charAddAB.push_back(number);
+                    number = '1';
+                    temp = '0';
+                }else if (temp == '0' && i == 0){
+                    number = '0';
+                    charAddAB.push_back(number);
+                    number = '1';
+                    temp = '0';
+                }else if(temp == '1'){
+                    number = '1';
+                    temp = '1';
+                }else if(temp == '0'){
+                    number = '0';
+                    temp = '1';
+                }
+            }else if(charArrA[i] == '1' && charArrB[i] == '0'){
+
+                if(temp == '1' && i == 0){
+                    number = '0';
+                    charAddAB.push_back(number);
+                    number = '1';
+                    temp = '0';
+                }else if(temp == '0' && i == 0){
+                    number = '1';
+                    temp = '0';
+                }else if(temp == '1'){
+                    number = '0';
+                    temp = '1';
+                }else if(temp == '0'){
+                    number = '1';
+                    temp = '0';
+                }
+            }else if(charArrA[i] == '0' && charArrB[i] == '1'){
+                if(temp == '1' && i == 0){
+                    number = '0';
+                    charAddAB.push_back(number);
+                    number = '1';
+                    temp = '0';
+                }else if(temp == '0' && i == 0){
+                    number = '0';
+                    temp = '0';
+                }else if(temp == '1'){
+                    number = '0';
+                    temp = '1';
+                }else{
+                    number = '1';
+                    temp = '0';
+                }
+            }else if(charArrA[i] == '0' && charArrB[i] == '0'){
+                if(temp == '1'){
+                    number = '1';
+                    temp = '0';
+                }else{
+                    number = '0';
+                    temp = '0';
+                }
+            }
+
+            charAddAB.push_back(number);
+        }
+
+        //reverse to normal, convert it to strings
+        string charA = "";
+        for(long long int i = charAddAB.size()-1; i > -1; i--){
+            charA = charA + charAddAB[i];
+        }
+
+        //convert it to integer
+        long long int AB = stoll(charA);
+
+        return AB;
+    }
+
+};
+
+int main(){
+    CalculateBinaryNumbers cbn;
+    std::cout << cbn.addBinaryNumbers("1011", "10111");
+}
