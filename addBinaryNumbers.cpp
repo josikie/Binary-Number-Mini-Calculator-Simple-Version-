5301c02f4c98484bbafa3077e58c12be622004fe
Josi Kie <kiejosi12@gmail.com>
2022-12-27 04:08:38 +0700

    Separated the utilities into functions.

--- addBinaryNumbers.cpp
+++ addBinaryNumbers.cpp
@@ -8,50 +8,25 @@ using namespace std;
 
 class CalculateBinaryNumbers{
     public:
-    long long int addBinaryNumbers(std::string a, std::string b){
-        // take the string lengths
-        long long int lengthA = a.length();
-        long long int lengthB = b.length();
 
-        // initiate pointer char array with size corresponding to the string lengths
-        char *arrA = new char[lengthA+1];
-        char *arrB = new char[lengthB+1];
-
-        // convert strings to char arrays
-        strcpy_s(arrA, lengthA+1, a.c_str());
-        strcpy_s(arrB, lengthB+1, b.c_str());
-
-        // convert char arrays to vector arrays
-        std::vector<char> charArrA;
-        for(long long int i = 0; i < lengthA; i++){
-            charArrA.push_back(arrA[i]);
+    std::vector<char> convertCharArrayToVector(char *arr, long long int length){
+        std::vector<char> charArr;
+        for(long long int i = 0; i < length; i++){
+            charArr.push_back(arr[i]);
         }
+        return charArr;
+    }
 
-        std::vector<char> charArrB;
-        for(long long int i = 0; i < lengthB; i++){
-            charArrB.push_back(arrB[i]);
-        }
-
-        // delete memory for char arrays
-        delete[] arrA;
-        delete[] arrB;
-
-        // add 0 in front if length B less than length A
-        if(lengthA > lengthB){
-            long long int devitation = lengthA - lengthB;
+    void equateLength(long long int firstLength, long long int secondLength, std::vector<char> &charArr){
+        if(firstLength > secondLength){
+            long long int devitation = firstLength - secondLength;
             for(long long int i = 0; i < devitation; i++){
-                charArrB.insert(charArrB.begin(), '0');
+                charArr.insert(charArr.begin(), '0');
             }
         }
+    }
 
-        if(lengthB > lengthA){
-            long long int devitation = lengthB - lengthA;
-            for(long long int i = 0; i < devitation; i++){
-                charArrA.insert(charArrA.begin(), '0');
-            }
-        }
-
-        // reverse, add binary strings
+    string addBinaryStrings(std::vector<char> charArrA, std::vector<char> charArrB){
         std::vector<char> charAddAB;
         char temp = '0';
         for(long long int i = charArrA.size()-1; i > -1; i--){
@@ -99,7 +74,7 @@ class CalculateBinaryNumbers{
                     number = '1';
                     temp = '0';
                 }else if(temp == '0' && i == 0){
-                    number = '0';
+                    number = '1';
                     temp = '0';
                 }else if(temp == '1'){
                     number = '0';
@@ -112,7 +87,7 @@ class CalculateBinaryNumbers{
                 if(temp == '1'){
                     number = '1';
                     temp = '0';
-                }else{
+                }else if(temp == '0'){
                     number = '0';
                     temp = '0';
                 }
@@ -126,6 +101,37 @@ class CalculateBinaryNumbers{
         for(long long int i = charAddAB.size()-1; i > -1; i--){
             charA = charA + charAddAB[i];
         }
+
+        return charA;
+    }
+    
+    long long int addBinaryNumbers(std::string a, std::string b){
+        // take the string lengths
+        long long int lengthA = a.length();
+        long long int lengthB = b.length();
+
+        // initiate pointer char array with size corresponding to the string lengths
+        char *arrA = new char[lengthA+1];
+        char *arrB = new char[lengthB+1];
+
+        // convert strings to char arrays
+        strcpy_s(arrA, lengthA+1, a.c_str());
+        strcpy_s(arrB, lengthB+1, b.c_str());
+
+        // convert char arrays to vector arrays
+        std::vector<char> charArrA{convertCharArrayToVector(arrA, lengthA)};
+        std::vector<char> charArrB{convertCharArrayToVector(arrB, lengthB)};
+
+        // delete memory for char arrays
+        delete[] arrA;
+        delete[] arrB;
+
+        // add 0 in front if length B less than length A
+        equateLength(lengthA, lengthB, charArrB);
+        equateLength(lengthB, lengthA, charArrA);
+
+        // reverse, add binary strings
+        string charA = addBinaryStrings(charArrA, charArrB);
 
         //convert it to integer
         long long int AB = 0;
@@ -144,5 +150,5 @@ class CalculateBinaryNumbers{
 
 int main(){
     CalculateBinaryNumbers cbn;
-    std::cout << cbn.addBinaryNumbers("1111111111111111111", "1010000111111");
+    std::cout << cbn.addBinaryNumbers("111001", "11001");
 }
